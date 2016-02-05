b17e869a22dfc8afdc97b5abc236f7342376f528
Brian Gillespie <bng1290@gmail.com>
2016-02-04 19:01:32 -0800

    Added in some abstractions on the side. Starting with mocks

--- EZOIRoutinesMaven-10-19-2015/src/main/java/CheckoutCode/EZOIRoutinesMaven/RestrictCheckIn.java
+++ EZOIRoutinesMaven-10-19-2015/src/main/java/CheckoutCode/EZOIRoutinesMaven/RestrictCheckIn.java
@@ -9,60 +9,12 @@ import java.net.URL;
 import java.util.ArrayList;
 
 import org.codehaus.jackson.JsonFactory;
+import org.codehaus.jackson.JsonParseException;
 import org.codehaus.jackson.JsonParser;
 import org.codehaus.jackson.JsonToken;
 
 public class RestrictCheckIn {
 	private static final int PAGES = 3;
-
-
-	// Currently being used for ad-hoc testing
-	public static void main(String[] args) throws Exception {
-		RestrictCheckIn r = new RestrictCheckIn();
-		
-		// Begin ad-hoc testing for getAssetState
-		int assetID = 2;
-		System.out.println(r.getAssetState(assetID));
-
-		String urlState =
-				"https://northeasternuniversitysea.ezofficeinventory.com/" + "assets/" + assetID + ".api";
-		String response = r.getAPIResponseFromURL(urlState);
-		String state = "";
-
-		JsonFactory jsonFactory = new JsonFactory();
-		JsonParser parser = jsonFactory.createJsonParser(response);
-
-		while (parser.nextToken() != JsonToken.END_OBJECT) {
-			String token = parser.getText();
-
-			if ("state".equals(token)) {
-				parser.nextToken();
-				state = parser.getText();
-			}
-		}
-		System.out.println(state);
-
-		// Begin ad-hoc testing for getAllActiveAssetIDs
-		ArrayList<Integer> allAssetIDs = new ArrayList<Integer>();
-
-		for (int i = 1; i <= PAGES; i++){
-			String urlIDs = "https://northeasternuniversitysea.ezofficeinventory.com/assets.api?page=" + i;
-			String responseTwo = r.getAPIResponseFromURL(urlIDs);
-
-			JsonFactory jsonFactoryTwo = new JsonFactory();
-			JsonParser parserTwo = jsonFactoryTwo.createJsonParser(responseTwo);
-
-			while (parserTwo.nextToken() != JsonToken.END_ARRAY) {
-				String token = parserTwo.getText();
-				if ("sequence_num".equals(token)) {
-					parserTwo.nextToken();
-					allAssetIDs.add(parserTwo.getValueAsInt());
-				}
-			}
-		}
-		System.out.println(r.getAllActiveAssetIDs().toString());
-		System.out.println(allAssetIDs.toString());
-	}
 
 	// curl command for getting all asset details:
 	// curl -H "token:<COMPANY_TOKEN>" -X GET \
@@ -148,6 +100,13 @@ public class RestrictCheckIn {
 		}
 		return state;
 	}
+	
+	public String getAssetState2(int assetID) throws IOException{
+		String urlState =
+				"https://northeasternuniversitysea.ezofficeinventory.com/" + "assets/" + assetID + ".api";
+		String response = this.getAPIResponseFromURL(urlState);
+		return this.getStateFromJSONString(response);
+	}
 
 	/**
 	 * 
@@ -155,44 +114,144 @@ public class RestrictCheckIn {
 	 * @return response - A string representation of a JSON response from the API
 	 * @throws IOException
 	 */
-	protected String getAPIResponseFromURL(String targetURL) throws IOException{
-
+	public String getAPIResponseFromURL(String targetURL) throws IOException{
 		URL obj;
 		String response = "";
 		try {
 			obj = new URL(targetURL);
-			HttpURLConnection con = null;
-			try {
-				con = (HttpURLConnection) obj.openConnection();
-				
-				// request method
-				con.setRequestMethod("GET");
-
-				// request headers
-				con.setRequestProperty("token", Constants.USER_TOKEN);
-				con.setRequestProperty("User-Agent", Constants.USER_AGENT);
-			}catch (IOException e) {
-				throw new IOException("Cannot connect to the given URL.", e);
-			}finally{
-				if (con != null) con.disconnect();
-			}
-			// reading input stream
-			BufferedReader in = null;
-			try{
-				in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-				String inputLine;
-				while ((inputLine = in.readLine()) != null) {
-					response += inputLine;
-				}
-				in.close();
-			}catch (IOException e){
-				throw new IOException("Cannot interpret input stream from the given URL", e);
-			}finally {
-				if (in != null) in.close();
-			}
+			HttpURLConnection con = configureGETRequest(obj);
+			response = getInputStreamAsString(con);
 		}catch (MalformedURLException e) {
 			e.printStackTrace();
 		}
 		return response;
+	}
+
+	/**
+	 * 
+	 * @param apiCallTargetURL - URL Object representing the URL for an API call to EZOI
+	 * @return connection - An HttpURLConnection that allows access to the JSON response from EZOI
+	 * @throws IOException
+	 */
+	private static HttpURLConnection configureGETRequest(URL apiCallTargetURL) throws IOException{
+		HttpURLConnection connection = null;
+		try {
+			connection = (HttpURLConnection) apiCallTargetURL.openConnection();
+
+			// request method
+			connection.setRequestMethod("GET");
+
+			// request headers
+			connection.setRequestProperty("token", Constants.USER_TOKEN);
+			connection.setRequestProperty("User-Agent", Constants.USER_AGENT);
+		}catch (IOException e) {
+			throw new IOException("Cannot connect to the given URL.", e);
+		}finally{
+			if (connection != null) connection.disconnect();
+		}
+		return connection;
+	}
+
+	/**
+	 * 
+	 * @param connection - An HttpURLConnection to the EZOI API
+	 * @return response - the String representation of the API response
+	 * @throws IOException
+	 */
+	private static String getInputStreamAsString(HttpURLConnection connection) throws IOException{
+		BufferedReader in = null;
+		String responseString = "";
+		try{
+			in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+			String inputLine;
+			while ((inputLine = in.readLine()) != null) {
+				responseString += inputLine;
+			}
+			in.close();
+		}catch (IOException e){
+			throw new IOException("Cannot interpret input stream from the given URL", e);
+		}finally {
+			if (in != null) in.close();
+		}
+		return responseString;
+	}
+
+	/**
+	 * 
+	 * @param response
+	 * @return
+	 * @throws JsonParseException
+	 * @throws IOException
+	 */
+	public String getStateFromJSONString(String response) throws JsonParseException, IOException{
+		JsonFactory jsonFactory = new JsonFactory();
+		JsonParser parser = jsonFactory.createJsonParser(response);
+		String state = "";
+		while (parser.nextToken() != JsonToken.END_OBJECT) {
+			String token = parser.getText();
+
+			if ("state".equals(token)) {
+				parser.nextToken();
+				state = parser.getText();
+			}
+		}
+		return state;
+	}
+
+	/**
+	 * 
+	 * @param response
+	 * @return
+	 * @throws JsonParseException
+	 * @throws IOException
+	 */
+	public static ArrayList<Integer> getActiveAssetsFromPage(String response) throws JsonParseException, IOException{
+		ArrayList<Integer> assetIDs = new ArrayList<Integer>();
+
+		JsonFactory jsonFactory = new JsonFactory();
+		JsonParser parser = jsonFactory.createJsonParser(response);
+
+		while (parser.nextToken() != JsonToken.END_ARRAY) {
+			String token = parser.getText();
+			if ("sequence_num".equals(token)) {
+				parser.nextToken();
+				assetIDs.add(parser.getValueAsInt());
+			}
+		}
+		return assetIDs;
+	}
+
+
+	// Currently being used for ad-hoc testing
+	public static void main(String[] args) throws Exception {
+		RestrictCheckIn r = new RestrictCheckIn();
+
+
+
+		// Begin ad-hoc testing for getAssetState
+		//System.out.println(r.getAssetState(assetID));
+
+		int assetID = 2;
+		String urlState =
+				"https://northeasternuniversitysea.ezofficeinventory.com/" + "assets/" + assetID + ".api";
+		String response = r.getAPIResponseFromURL(urlState);
+		System.out.println(response);
+		String state = r.getStateFromJSONString(response);
+		System.out.println(state);
+
+
+
+		// Begin ad-hoc testing for getAllActiveAssetIDs
+		//System.out.println(r.getAllActiveAssetIDs().toString());
+
+		ArrayList<Integer> allAssetIDs = new ArrayList<Integer>();
+		for (int i = 1; i <= PAGES; i++){
+			String urlIDs = "https://northeasternuniversitysea.ezofficeinventory.com/assets.api?page=" + i;
+			String responseTwo = r.getAPIResponseFromURL(urlIDs);
+
+			allAssetIDs.addAll(getActiveAssetsFromPage(responseTwo));
+		}
+
+		System.out.println(allAssetIDs.toString());
 	}
 }
